e03a4221d0422c4dc0a6a2029e7b16ad2a8cfaf4
Hemamalini101198 <150349833+Hemamalini101198@users.noreply.github.com>
2023-11-29 12:29:30 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -5,13 +5,6 @@ terraform {
       version = "3.50.0"
     }
   }
-
-  #adding s3 bucket for remote state storage
-  backend "s3"{
-    bucket = "tf-statefiles-bucket"
-    key = "iam/${var.user_name}/terraform.tfstate"
-    region = "ap-south-1"
-  }
 }
 
 provider "aws" {
