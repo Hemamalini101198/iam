844703beb1471e5a303b18c8f7c7878210c21a3e
Hemamalini101198 <150349833+Hemamalini101198@users.noreply.github.com>
2023-11-29 12:53:59 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,17 +1,11 @@
 terraform {
-  required_providers {
-    aws = {
-      source = "hashicorp/aws"
-      version = "3.50.0"
-    }
-  }
   backend "s3" {
-    bucket = "tf-statefiles-bucket"
-    key    = "iam/${terraform.workspace}/terraform.tfstate"
-    region = "ap-south-1"
+    key            = "iam/${terraform.workspace}/terraform.tfstate"
+    region         = "your-region"
+    encrypt        = true
   }
 }
-
+ 
 provider "aws" {
-  region = "ap-south-1"
+  region = "your-region"
 }
