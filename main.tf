935016722e4a694c050a3de8dd3f862cb75d2f27
Hemamalini101198 <150349833+Hemamalini101198@users.noreply.github.com>
2023-11-29 12:41:18 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,26 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "3.50.0"
+    }
+  }
+
+# backend-config.tf
+
+variable "user_name" {}
+
+locals {
+  key_prefix = "iam/${var.user_name}"
+}
+
+backend "s3" {
+  bucket = "tf-statefiles-bucket"
+  key    = local.key_prefix != "" ? "${local.key_prefix}/terraform.tfstate" : "terraform.tfstate"
+  region = "ap-south-1"
+}
+}
+
 #iam user creation
 resource "aws_iam_user" "iam-user" {
   name = "${var.user_name}"
