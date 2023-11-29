c1dd6de08cce5f06197ca7b1da6dd61d41204332
Hemamalini101198 <150349833+Hemamalini101198@users.noreply.github.com>
2023-11-29 12:30:51 +0530

    Create backend-config.tf

---
+++ backend-config.tf
@@ -0,0 +1,13 @@
+# backend-config.tf
+
+variable "user_name" {}
+
+locals {
+  key_prefix = "iam/${var.user_name}"
+}
+
+backend "s3" {
+  bucket = "tf-statefiles-bucket"
+  key    = local.key_prefix != "" ? "${local.key_prefix}/terraform.tfstate" : "terraform.tfstate"
+  region = "ap-south-1"
+}
