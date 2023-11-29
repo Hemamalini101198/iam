3d33d306a0890b98947bb67f76260908769cf132
Hemamalini101198 <150349833+Hemamalini101198@users.noreply.github.com>
2023-11-29 13:06:52 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -9,12 +9,6 @@ terraform {
       version = "3.50.0"
     }
   }
-
-  backend "s3" {
-    bucket = "tf-statefiles-bucket"
-    key    = local.key_prefix != "" ? "${local.key_prefix}/terraform.tfstate" : "terraform.tfstate"
-    region = "ap-south-1"
-  }
 }
 
 #iam user creation
